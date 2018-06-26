729298118834226516fb7ec046b242ef361591ee
Tocknicsu <wingemerald@gmail.com>
2018-06-26 10:41:42 +0800

    let id force true and preRead -> readable

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-import {
-  defaultFieldResolver
-} from 'graphql'
-import { Cache } from 'memory-cache'
+import { defaultFieldResolver } from "graphql"
+import { Cache } from "memory-cache"
 
-import _ from 'lodash'
-import uuid from 'uuid'
-import defaults from 'defaults'
-import waitUntil from 'wait-until-promise'
+import _ from "lodash"
+import uuid from "uuid"
+import defaults from "defaults"
+import waitUntil from "wait-until-promise"
 
 const funcWrapper = async (obj, args) => {
-  if (typeof obj === 'function') {
+  if (typeof obj === "function") {
     return await obj(...args)
   }
   return obj
 }
 
 export const RuleHelper = (rules, fields) => {
-  return _.mapValues(fields, (field) => {
+  return _.mapValues(fields, field => {
     return {
       ...field,
-      resolve: rules(field.resolve)
+      resolve: rules(field.resolve),
     }
   })
 }
 
-export default (config) => {
+export default config => {
   // cache will following this rule
   const cachePool = new Cache()
   config = defaults(config, {
@@ -33,67 +31,85 @@ export default (config) => {
     cache: {
       enable: true,
       key: uuid.v4(),
-      expire: 60000
+      expire: 60000,
     },
     defaultRule: {
-      preRead: true
-    }
+      readable: true,
+    },
   })
   let cacheConfig = defaults(config.cache, {
     enable: true,
     key: uuid.v4(),
-    expire: 60000
+    expire: 60000,
   })
-  return (func) => {
+  config.rules.id = true
+  return func => {
     // custom resolver or defaultFieldResolver
     const resolverFunc = func ? func : defaultFieldResolver
     return async (...resolverFuncArgs) => {
       let props = null
       // check enable cache or not
       if (cacheConfig.enable) {
-        cacheConfig.expire = await funcWrapper(cacheConfig.expire, resolverFuncArgs)
+        cacheConfig.expire = await funcWrapper(
+          cacheConfig.expire,
+          resolverFuncArgs,
+        )
         // get the cache
         const cacheKey = await funcWrapper(cacheConfig.key, resolverFuncArgs)
         let cache = cachePool.get(cacheKey)
         if (cache === null) {
           cachePool.put(cacheKey, {
             props: {},
-            done: false
+            done: false,
           })
           props = {}
           for (let propsKey in config.props) {
-            props[propsKey] = await funcWrapper(config.props[propsKey], resolverFuncArgs)
+            props[propsKey] = await funcWrapper(
+              config.props[propsKey],
+              resolverFuncArgs,
+            )
           }
-          cachePool.put(cacheKey, {
-            done: true,
-            props
-          }, cacheConfig.expire)
+          cachePool.put(
+            cacheKey,
+            {
+              done: true,
+              props,
+            },
+            cacheConfig.expire,
+          )
         } else {
-          await waitUntil(() => {
-            cache = cachePool.get(cacheKey)
-            return cache.done
-          }, cacheConfig.expire, 1)
+          await waitUntil(
+            () => {
+              cache = cachePool.get(cacheKey)
+              return cache.done
+            },
+            cacheConfig.expire,
+            1,
+          )
           props = cache.props
         }
       } else {
         props = {}
         for (let propsKey in config.props) {
-          props[propsKey] = await funcWrapper(config.props[propsKey], resolverFuncArgs)
+          props[propsKey] = await funcWrapper(
+            config.props[propsKey],
+            resolverFuncArgs,
+          )
         }
       }
       // get config
       let rule
       const info = resolverFuncArgs[3]
       if (info.fieldName in config.rules) {
-         rule = config.rules[info.fieldName]
-        if (typeof rule === 'boolean') {
-        } else if (typeof rule === 'function') {
+        rule = config.rules[info.fieldName]
+        if (typeof rule === "boolean") {
+        } else if (typeof rule === "function") {
           rule = await funcWrapper(rule, [props])
         } else {
           rule = true
         }
       } else {
-        rule = await funcWrapper(config.defaultRule.preRead, [props])
+        rule = await funcWrapper(config.defaultRule.readable, [props])
       }
       if (rule) {
         return resolverFunc(...resolverFuncArgs)
